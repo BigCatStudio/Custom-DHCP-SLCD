b19775bbee25f813c9229d7c261d72f9cbe6941c
Piotr <piotr.bud01@wp.pl>
2024-01-02 23:00:58 +0100

    Added proper comments

--- src/utilities.py
+++ src/utilities.py
@@ -2,11 +2,9 @@ from ipaddress import IPv4Network, IPv4Address, AddressValueError, NetmaskValueE
 
 
 class IP_Pool:
-    @staticmethod
+    @staticmethod       # Those functions are static to allow using them in other parts of system (client and server)
     def check_ip(ip_address):
         try:
-            # TODO check if it is not broadcast or network address
-            # print(f"Ip address is valid: {ip_address}")
             IPv4Address(ip_address)    # Raises exception if does not initialize
             return True
         except AddressValueError:
@@ -31,20 +29,19 @@ class IP_Pool:
         if IP_Pool.check_ip_pool(ip_start, ip_end) and IP_Pool.check_subnet_mask(subnet_mask):
             self.ip_start = ip_start
             self.ip_end = ip_end
-            self.subnet_mask = subnet_mask
             self.ip_current = IPv4Address(self.ip_start)
+            self.subnet_mask = subnet_mask
             self.ip_list = []
         else:
             raise ValueError
-            # TODO should raise exception
 
     def __iter__(self):
         self.ip_current = IPv4Address(self.ip_start)
         return self
 
-    def __next__(self):
-        if len(self.ip_list) == len(self):
-            return None     # TODO it should call mechanism to free IP address from active client
+    def __next__(self):     # Iterating over all possible addresses in range and returning to first one if it reaches end
+        if len(self.ip_list) == len(self):      # All addresses are alocated
+            return None
 
         while self.ip_current in self.ip_list:
             if self.ip_current == IPv4Address(self.ip_end):
@@ -53,7 +50,6 @@ class IP_Pool:
                 self.ip_current += 1
 
         self.ip_list.append(self.ip_current)
-
         return str(self.ip_current)
 
     def __len__(self):
@@ -71,8 +67,8 @@ def get_option_value(option_list, option):
 
 
 def mac_to_bytes(mac_address):
-    return bytes.fromhex(mac_address.replace(':', ''))  # Remove any delimiters (like ':') from the MAC address and convert it to bytes
+    return bytes.fromhex(mac_address.replace(':', ''))
 
 
 def bytes_to_mac(mac_address):
-    return ":".join("{:02x}".format(byte) for byte in mac_address)  # Convert bytes back to MAC address format with colon separators
+    return ":".join("{:02x}".format(byte) for byte in mac_address)
